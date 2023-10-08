862f4aca0be45964983f841acf2512140e209c84
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-10-08 22:34:31 +0700

    ADD NumberingSection components

---
+++ src/views/AboutMeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import NumberingSection from '../components/numberingSection.vue'
+</script>
+
+<template>
+  <div class="min-h-screen lg:px-28 pt-28">
+    <NumberingSection>
+      <template #number>
+        1.
+      </template>
+      <template #title>
+        About Me!
+      </template>
+    </NumberingSection>
+  </div>
+</template>
