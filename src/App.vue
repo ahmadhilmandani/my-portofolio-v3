07c909ab6c2306ad96d98c19d2eed982472ccef6
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2023-10-08 21:54:37 +0700

    ADD home pages

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,60 @@
-<script>
-import { RouterLink, RouterView } from 'vue-router'
+<script setup>
+import { RouterLink } from 'vue-router'
+import { IconMail, IconBrandGithub, IconBrandLinkedin, IconBrandInstagram } from '@tabler/icons-vue'
+import HomeView from './views/HomeView.vue'
+
+function setIconColorBlack(elementId) {
+  document.getElementById(elementId).setAttribute('stroke', '#23252A')
+}
+
+function setIconColorBeige(elementId) {
+  document.getElementById(elementId).setAttribute('stroke', '#F5F4EF')
+}
+
 </script>
 
 <template>
+  <!-- aside navigation -->
+  <aside class="w-full fixed bottom-0 px-8 flex justify-between items-end z-[1]">
+
+    <div class="grid grid-rows-4 grid-cols-1 gap-7">
+      <a href="mailto:ahmadhilmanlagi@gmail.com" target="_blank"
+        class="bg-custom-beige-100 rounded-lg p-2 hover:bg-gradient-to-r hover:from-custom-green-100 hover:to-custom-green-50 hover:shadow-lg hover:shadow-custom-green-100/50 shadow- hover:-translate-y-1 transition-all"
+        @mouseenter="() => { setIconColorBlack('email') }" @mouseleave="() => { setIconColorBeige('email') }">
+        <IconMail size="20" stroke-width="2" color="#F5F4EF" id="email" />
+      </a>
+      <a href="https://github.com/ahmadhilmandani" target="_blank"
+        class="bg-custom-beige-100 rounded-lg p-2 hover:bg-gradient-to-r hover:from-custom-green-100 hover:to-custom-green-50 hover:shadow-lg hover:shadow-custom-green-100/50 shadow- hover:-translate-y-1 transition-all"
+        @mouseenter="() => { setIconColorBlack('github') }" @mouseleave="() => { setIconColorBeige('github') }">
+        <IconBrandGithub size="20" stroke-width="2" color="#F5F4EF" id="github" />
+      </a>
+      <a href="https://www.linkedin.com/in/ahmad-hilman-dani/" target="_blank"
+        class="bg-custom-beige-100 rounded-lg p-2 hover:bg-gradient-to-r hover:from-custom-green-100 hover:to-custom-green-50 hover:shadow-lg hover:shadow-custom-green-100/50 shadow- hover:-translate-y-1 transition-all"
+        @mouseenter="() => { setIconColorBlack('linkedin') }" @mouseleave="() => { setIconColorBeige('linkedin') }">
+        <IconBrandLinkedin size="20" stroke-width="2" color="#F5F4EF" id="linkedin" />
+      </a>
+      <a href="https://www.instagram.com/ahmadhilmandani/" target="_blank"
+        class="bg-custom-beige-100 rounded-lg p-2 hover:bg-gradient-to-r hover:from-custom-green-100 hover:to-custom-green-50 hover:shadow-lg hover:shadow-custom-green-100/50 shadow- hover:-translate-y-1 transition-all"
+        @mouseenter="() => { setIconColorBlack('instagram') }" @mouseleave="() => { setIconColorBeige('instagram') }">
+        <IconBrandInstagram size="20" stroke-width="2" color="#F5F4EF" id="instagram" />
+      </a>
+      <div class="w-[1.8px] h-56 bg-custom-beige-100 mx-auto"></div>
+    </div>
+
+    <div class="grid grid-rows-1 grid-cols-1 gap-7">
+      <div class="vertical-text text-custom-beige-50 rounded-lg px-2 py-4 bg-custom-beige-100" @click="greet">
+        Portofolio website of Ahmad Hilman Dani
+      </div>
+      <div class="w-[1.8px] h-56 bg-custom-beige-100 mx-auto"></div>
+    </div>
+
+  </aside>
+
+  <!-- top navigation -->
   <nav
-    class="w-full h-[70px] flex justify-between items-center lg:px-20 bg-custom-beige-50 border-b border-b-custom-black-50 fixed top-0 left-0 right-0 z-[1000]">
+    class="w-full h-[60px] flex justify-between items-center lg:px-20 bg-custom-beige-50 border-b border-b-custom-black-50 fixed top-0 left-0 right-0 z-[1000]">
     <div class="flex items-center gap-2">
-      <svg width="21" height="39" viewBox="0 0 21 39" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg width="16" height="33" viewBox="0 0 21 39" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path fill-rule="evenodd" clip-rule="evenodd"
           d="M4.47754 7.82878e-07L0 0L-3.05609e-06 17.4788L20.9745 17.4788L20.9746 3.66731e-06L16.5538 2.89436e-06L16.5538 13.5458L4.47754 13.5458L4.47754 7.82878e-07Z"
           fill="#23252A" />
@@ -21,9 +69,19 @@ import { RouterLink, RouterView } from 'vue-router'
       <RouterLink to="/about">About Me</RouterLink>
       <RouterLink to="/about">Experience</RouterLink>
       <a href=""
-        class="lg:px-6 lg:py-3 rounded-full bg-gradient-to-r from-custom-green-100 to-custom-green-50 font-semibold uppercase text-sm">Contact</a>
+        class="lg:px-4 lg:py-2 rounded-full bg-gradient-to-r from-custom-green-100 to-custom-green-50 font-semibold uppercase text-sm hover:shadow-lg hover:shadow-custom-green-100/50 shadow- hover:-translate-y-1 transition-all">Contact</a>
     </div>
   </nav>
 
-  <RouterView />
+  <!-- main contenct -->
+  <main>
+    <HomeView />
+  </main>
 </template>
+
+<style scoped>
+.vertical-text {
+  writing-mode: vertical-lr;
+  text-orientation: mixed;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="min-h-screen">
+  <div class="w-full min-h-screen flex flex-col justify-between items-center pt-24 relative">
+    <div>
+      <h3 class="relative z-[5] text-center mt-1 text-xl font-normal">Hi👋 I’m Ahmad Hilman Dani</h3>
+      <h1 class="relative z-[5] text-center mt-1 text-[56px]">Frontend Developer</h1>
+      <h2 class="relative z-[5] text-center mt-1 text-[56px] text-white border-text">& UI Designer</h2>
+    </div>
+    <img src="../assets/images/myPhotoLight.png" class="absolute bottom-0 z-[1] w-[340px]" alt="">
+    <div
+      class="flex justify-center items-center gap-x-7 relative z-[5] bg-custom-beige-50 border-t border-x border-custom-black-50 rounded-t-[40px] py-5 px-12">
+      <div class="flex justify-center items-center gap-2 pr-4">
+        <h4 class="text-4xl">1</h4>
+        <p>Year using React Js <br> or Vue Js</p>
+      </div>
+      <div class="h-7 w-[0.5px] bg-custom-black-50"></div>
+      <div class="flex justify-center items-center gap-2 pr-4">
+        <h4 class="text-4xl">1</h4>
+        <p>Year in <br>UI Design</p>
+      </div>
+      <div class="h-7 w-[0.5px] bg-custom-black-50"></div>
+      <div class="flex justify-center items-center gap-2 pr-4">
+        <h4 class="text-4xl">3</h4>
+        <p>Years in hope that figma <br> would still be free after the <br> Adobe acquisition 😁</p>
+      </div>
+    </div>
+  </div>
 
+  <div class="w-full flex flex-col justify-center items-center py-16 px-40 bg-custom-black-50">
+    <h1 class="text-custom-beige-50 font-normal text-2xl text-center leading-[175%]">
+      “ Saya diberikan kenikmatan oleh Allah ilmu technology sehingga saya bisa membuat pesawat terbang, tapi sekarang
+      saya
+      tahu bahwa <span class="text-custom-green-100 font-semibold text-2xl">ilmu agama itu lebih bermanfaat untuk
+        umat.</span> Kalo saya disuruh memilih antara keduanya maka <span
+        class="text-custom-green-100 font-semibold text-2xl">saya akan
+        memilih ilmu Agama.</span>”
+    </h1>
+    <div class="mt-6 italic text-[16px] text-custom-beige-50">- Bacharuddin Jusuf Habibie -</div>
   </div>
 </template>
+
+<style>
+.border-text {
+  text-shadow: 0 0 1.5px #23252A
+}
+</style>
